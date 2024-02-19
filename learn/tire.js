d88a86725c71a6721a890cc5b6bd45320f505444
cue28 <siyoonjeonn@gmail.com>
2024-02-19 17:43:27 +0900

    240218-19

--- learn/tire.js
+++ learn/tire.js
@@ -36,3 +36,42 @@ function solution(words) {
 
   return answer; // 반환
 }
+
+/*Trie 구현
+
+class Node {
+    constructor(value = ""){
+        this.value = value;
+        this.children = new Map();
+    }
+}
+
+class Trie {
+    constructor(){
+        this.root = new Node();
+    }
+
+    has(string){
+        let currentNode = this.root;
+
+        for(const char of string){
+            if(!currentNode.children.has(char)){
+                return false
+            }
+            currentNode = currentNode.children.get(char);
+        }
+        return true;
+    }
+
+    insert(string){
+        let currentNode = this.root;
+
+        for(const char of string){
+            if(!currentNode.children.has(char)){
+                currentNode.children.set(char , new Node(currentNode.value + char))
+            }
+            currentNode = currentNode.children.get(char)
+        }
+    }
+}
+*/
